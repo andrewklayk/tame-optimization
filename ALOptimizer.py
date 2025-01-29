aefe56ffc1a474a1a64a05e2aa4cf05b7ac0efe5
andrewklayk <kliachkin.andrii@gmail.com>
2025-01-29 21:38:43 +0100

    eheh

---
+++ ALOptimizer.py
@@ -0,0 +1,90 @@
+import torch.utils.data.dataloader
+import torch.optim as optim
+
+
+class ALOptimizer:
+    def __init__(self,
+                 net: torch.nn.Module,
+                 loss_fn: callable,
+                 m: int,
+                 lambda_0: torch.Tensor=None,
+                 aug_term: float=1.,
+                 t: float=1.01,
+                 constraint_fn: callable=None,
+                 minimizer: str='Adam',
+                 **kwargs):
+        """
+        Solves an inequality-constrained optimization problem using the non-smooth Augmented Lagrangian method:
+        min f(x) s.t. c(x) <= 0
+
+        Args:
+            net ()
+            # params (iterable): iterable of parameters to optimize.
+            loss_fn (callable): the objective function.
+            m (int): number of inequalities.
+            lambda_0 (torch.Tensorm, optional): initial value for the Lagrange multipliers, expected to be of shape (m,).
+            aug_term (float): initial value for the r (rho) parameter.
+            t (float): multiplier for the r (rho) parameter.
+            constraint_fn (callable, optional): the constraint function, expected: f(net) -> Tensor((m,)) 
+            expected to return a tensor of constraint values.
+            minimizer (str, optional): the PyTorch optimizer to minimize the objective function on each iteration; default is Adam.
+            **kwargs: arguments for the minimizer.
+        """
+        # self._params = params
+        self.net = net
+        
+        self._loss_fn = loss_fn
+        self._constraint_fn = constraint_fn
+        self._m = m
+        self._lambda = lambda_0 if lambda_0 is not None else torch.ones(m)
+        self._ss = aug_term
+        self._t = t
+
+        ## TODO: kwargs
+        
+        if minimizer is None or minimizer.lower() == 'adam':
+            self._optimizer = optim.Adam(self.net.parameters(), lr=0.005)
+        elif minimizer.lower() == 'sgd':
+            self._optimizer = optim.SGD(self.net.parameters(), lr =0.005)
+        else:
+            raise ValueError(f'Unknown optimizer: {minimizer}')
+
+
+        
+    def optimize(self, dataloader: torch.utils.data.DataLoader, maxiter=3, epochs=3, verbose=True):
+        self.loss_val = 0
+        self.history = {'L': [], 'loss': [], 'constr': []}
+        for k in range(maxiter):
+            # minimize the augmented lagrangian with the current multiplier values
+            for epoch in range(epochs):    
+                for i, data in enumerate(dataloader):
+                    self._optimizer.zero_grad()
+                    inputs, labels = data
+                    outputs = self.net(inputs)
+                    constraint_eval = self._constraint_fn(self.net)
+                    loss_eval = self._loss_fn(outputs, labels)
+                    
+                    L = loss_eval + self._lambda @ constraint_eval + 0.5*self._ss*torch.sum(torch.square(constraint_eval))
+                    L.backward()
+                    self._optimizer.step()
+
+                    ###
+                    print(f'{epoch}, {i}, {loss_eval.detach().item()}, {constraint_eval.detach().item()}', end='\r')
+                    ###
+
+                    self.history['L'].append(L)
+                    self.history['loss'].append(loss_eval)
+                    self.history['constr'].append(constraint_eval)
+                
+            with torch.no_grad():
+                constr = self._constraint_fn(self.net)
+                if verbose:
+                    print(constr)
+                    print(self._loss_fn(outputs, labels))
+                    print('\n')
+                self._lambda += self._ss*constr
+                self._ss *= self._t
+
+        #######################
+        # stopping condition? #
+        #######################
